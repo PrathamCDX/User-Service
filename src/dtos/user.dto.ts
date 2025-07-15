f2b053f0e7d3fa9cf4373e0e7ce2c51b449bb14f
Arijit Ganguly <aganguly158@gmail.com>
2025-07-15 15:46:34 +0530

    #feat: Created RegisterUserDto and LoginUserDto

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,11 @@
+export type RegisterUserDto = {
+    fullName: string
+    email: string
+    password: string
+    honeNo: string
+}
+
+export type LoginUserDto = {
+    email: string
+    password: string
+}
